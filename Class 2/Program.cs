b1233205b94b8870a624fc713131814d254fb68c
charan1805 <sreecharanmudhiraj@gmail.com>
2023-07-18 11:32:22 +0530

    First Commit

---
+++ Class 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_2
+{
+
+namespace ConAppEnum
+    {
+        internal class Program
+        {
+            public enum DayofWeek
+            {
+                Monday = 1, Tuesday, Wednesday, Thursday, Friday, Saturday
+            }
+            static void Main(string[] args)
+            {
+                DayofWeek today = DayofWeek.Wednesday;
+                Console.WriteLine("Day:" + today);
+                Console.WriteLine("Day Number " + (int)today);
+                Console.ReadKey();
+            }
+        }
+    }
+}
+    
+
